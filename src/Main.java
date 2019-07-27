9d0f6338974f80f334d7a62981fb0017599bf96d
Elzada <68769904segizbaevu@gmail.com>
2019-07-27 22:53:39 +0600

    Закончила проект

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,25 @@ public class Main {
         names[0] = "Miraida";
         names[1] = "Zharkynay";
         names[2] = "Elena";
-        for (String j : names) ;
+
+        for (String justcoffee : names) {
+
+            switch (justcoffee) {
+                case "Miraida":
+
+                    System.out.println("Доброе утро," + justcoffee + "!");
+                    break;
+                case "Zharkynay":
+
+                    System.out.println("Доброе день,"  + justcoffee + "!");
+                    break;
+                case "Elena":
+
+                    System.out.println("Доброе вечер," + justcoffee + "!");
+                    break;
 
 
-
-
-
+            }
         }
     }
 }
