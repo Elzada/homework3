8c9aaef43cd9b399d5ec6fc5274bf0b1022c1d32
Elzada <68769904segizbaevu@gmail.com>
2019-07-27 22:32:19 +0600

    homework3

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String names[] = new String[3];
+        names[0] = "Miraida";
+        names[1] = "Zharkynay";
+        names[2] = "Elena";
+        for (String j : names) ;
+
+        switch (j) {
+            case "Miraida";
+                System.out.println();
+        }
+    }
+}
+
+
+
